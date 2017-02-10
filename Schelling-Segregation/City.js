cabac74c7e96ce961d707077db4492c942581a4c
Brice Fernandes <brice@sigmaforge.com>
2017-02-10 12:05:53 +0000

    Work on main tick for simulated city

--- Schelling-Segregation/City.js
+++ Schelling-Segregation/City.js
@@ -5,15 +5,27 @@ function random_pick(arr){
   return arr[Math.floor(Math.random()*arr.length)];
 }
 
+/*
+ * function random_pick chooses the type of household at random, selecting x, y
+ * or false. It does this by picking a random number from arr.length. In this
+ * example the array has three values of x, y or false. The random number could
+ * be any number from 0 to 3 (in this example). Firstly, we need a whole number
+ * since the index of the array is a whole number. Secondly, we need to floor
+ * the number rather than round it up as there is no index 3. So if the random
+ * number comes back as 2.6 we will floor it to 2. If the array is [x,y,false]
+ * it would pick false. Note: In the code below the values of the array are
+ * actually functions.
+ */
+
 function generate_grid(x,y, choices){
   var grid = [];
   for (var i = 0; i < y; i++) {
-    var arr = [];
+    var row = [];
     for (var j = 0; j < x; j++) {
-      var choice = random_pick(choices)
-      arr.push(choice());
+      var choice_fn = random_pick(choices)
+      row.push(choice_fn());
     }
-    grid.push(arr);
+    grid.push(row);
   }
   return grid;
 }
@@ -23,10 +35,10 @@ function print (stuff){
 }
 
 function City(x, y){
-  this.x = x;
-  this.y = y;
+  this.width = x;
+  this.height = y;
   this.residentFactories = [
-    ()=> new Household("x"),
+    ()=> new Household("x"), // equivalent to function(){ return new Household("x"); }
     ()=> new Household("o"),
     ()=> false
   ];
@@ -36,11 +48,8 @@ function City(x, y){
 City.prototype.print_city = function () {
   //iterate through grid x and y
   //print a dot if false
-  var width = this.grid[0].length;
-  var height = this.grid.length;
-
-  for (var y = 0; y <height; y++) {
-     for (var x = 0; x <width; x++) {
+  for (var y = 0; y <this.height; y++) {
+     for (var x = 0; x <this.width; x++) {
        var house = this.grid[y][x];
        if (!house) {
          print(" .");
@@ -52,5 +61,54 @@ City.prototype.print_city = function () {
   }
 }
 
+/**
+ * Will return an array of neighbours, which will not include vacant lots
+ */
+City.prototype.getNeighbours = function(address){
+  throw new Error("Not implemented");
+}
+
+/*
+ * give back an array with two numbers which will represent an empty plot within
+ * my grid.
+ */
+City.prototype.getRandomEmptyPlot = function(){
+  throw new Error("Not implemented");
+}
+
+/*
+ * it takes the household at the current address and moves it to the new address.
+ */
+City.prototype.move = function(currentAddress, newAddress){
+  throw new Error("Not implemented");
+}
+
+City.prototype.getHouseholdAtAddress = function(address){
+  // address will be in the form of an array [x,y]
+  var x = address[0];
+  var y = address[1];
+  return this.grid[y][x];
+}
+
+/**
+ * The tick function will go through every resident of the city, ask them how
+ * satisfied they are with their neighbours, and if the resident is dissatisfied,
+ * it will move them to a new random empty lot in the city.
+ */
+City.prototype.tick = function(){
+  for (var y = 0; y <this.height; y++) {
+     for (var x = 0; x <this.width; x++) {
+       var currentAddress = [x,y];
+       var current_household = this.getHouseholdAtAddress(currentAddress); // address -> Household|false
+       var neighbours = this.getNeighbours(currentAddress); // address -> [Households]
+       var satisfied = current_household.isSatisfied(neighbours); // [Households] -> Boolean
+       if (!satisfied) {
+         //move current_household to a randomly selected empty plot
+          var newAddress = this.getRandomEmptyPlot();
+          this.move(currentAddress, newAddress);
+       }
+     }
+   }
+}
 
 module.exports = City;
--- Schelling-Segregation/Household.js
+++ Schelling-Segregation/Household.js
@@ -3,13 +3,11 @@ var uuid = require("uuid");
 function Household(symbol, comfort_threshold){
   this.symbol = symbol;
   this.comfort = comfort_threshold || 0.3;
-  this.satisfaction = 0.5;
   this.uuid = uuid();
 }
 
-Household.prototype.get_satisfaction = function(){
-  // Will depend on the neighbours.
-  return this.satisfaction;
+Household.prototype.isSatisfied = function(neighbours){
+  throw new Error("Not implemented");
 }
 
 Household.prototype.toString = function(){
